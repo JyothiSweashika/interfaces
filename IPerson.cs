b0dcc0254b710fdeed1dab6f6d283a203e0eaa11
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-08-01 17:31:30 +0530

    interfaces

---
+++ IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+
+namespace Interfaces
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+}
